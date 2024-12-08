28de1a8fcd2cd132b57f3e63c2e7899cbd5a1b09
Matheus Coitinho <mhcl0303@gmail.com>
2024-12-08 20:24:06 -0300

    fix na rota de registrar usuario

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,12 +1,41 @@
 import prisma from '../../config/prisma';
 import { hashPassword } from '../../utils/hash';
 
-export const register = async (data: { name: string; email: string; password: string }) => {
-  const hashedPassword = await hashPassword(data.password);
-  return prisma.user.create({
+enum UserTypeEnum { 
+    CLIENTE = 'CLIENTE', 
+    ORGANIZADOR = 'ORGANIZADOR', 
+    FUNCIONARIO = 'FUNCIONARIO'
+}
+
+interface RegisterInput {
+  name: string;
+  email: string;
+  password: string;
+  userType: UserTypeEnum;
+}
+
+export const register = async(input: RegisterInput) => {
+  const { name, email, password, userType } = input;
+
+  const existingUser = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (existingUser) {
+    throw new Error('Usuário já existe com este e-mail.');
+  }
+
+  const hashedPassword = hashPassword(password);
+
+  const newUser = await prisma.user.create({
     data: {
-      ...data,
+      name,
+      email,
       password: hashedPassword,
+      userType,
+      createdAt: new Date(),
     },
   });
-};
+
+  return newUser;
+}
