f3c223909926e35d48dc53d5ea223bcffe7116e2
akiwnl <carvalhokaike69@gmail.com>
2024-12-11 21:26:09 -0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import userRoutes from './modules/user/user.routes';
 import authRoutes from './modules/auth/auth.routes';
 
@@ -7,7 +8,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use('/users', userRoutes);
 app.use('/auth', authRoutes);
 
