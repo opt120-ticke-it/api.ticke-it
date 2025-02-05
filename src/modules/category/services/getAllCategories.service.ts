c1291387ea5fd032e2362928a4e216fd1c09331a
Hanahadashi <felipefSuguimoto@gmail.com>
2025-02-05 17:59:55 -0300

    Listagem

--- src/modules/category/services/getAllCategories.service.ts
+++ src/modules/category/services/getAllCategories.service.ts
@@ -4,8 +4,16 @@ import { IGetAllCategories } from '../category.validation';
 class GetAllCategoriesService {
   async execute(params: IGetAllCategories) {
     const categories = await prisma.category.findMany({
+      orderBy: {
+        events: {
+          _count: 'desc', // Ordena pelo número de eventos em ordem decrescente
+        },
+      },
       skip: (params.page - 1) * params.limit,
       take: params.limit,
+      include: {
+        events: true,
+      },
     });
 
     return categories;
